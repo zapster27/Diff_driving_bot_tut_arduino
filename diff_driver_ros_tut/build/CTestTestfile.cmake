cd03371ec23299c5898cad7ef28a364b5928fc9a
zapster27 <tilzw27@gmail.com>
2022-05-30 22:21:51 +0530

    ros node init

---
+++ diff_driver_ros_tut/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/Documents/DiffDriveRobotCode_mk1/diff_driver_ros_tut/src
+# Build directory: /home/ubuntu/Documents/DiffDriveRobotCode_mk1/diff_driver_ros_tut/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("odom_publisher")
