0a7efe0a5ea4c0dfa32acfce84e3d74129cb76d7
tilan <tilzw27@gmail.com>
2022-05-30 22:37:48 +0530

    odom node init

--- diff_driver_ros_tut/src/odom_publisher/src/odom_publisher.cpp
+++ diff_driver_ros_tut/src/odom_publisher/src/odom_publisher.cpp
@@ -1,1 +1,20 @@
-#include<ros.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
+
+void odomCalculator(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "odom_pub");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("tick_msg", 1000, odomCalculator);
+
+
+  ros::spin();
+
+  return 0;
+}
