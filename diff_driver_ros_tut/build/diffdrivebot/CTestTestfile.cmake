72479cd06548958e8b694dd4ce456d076406f9f4
zapster27 <tilzw27@gmail.com>
2022-06-01 06:21:07 -0700

    URDF init

---
+++ diff_driver_ros_tut/build/diffdrivebot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Documents/Diff_driving_bot_tut_arduino/diff_driver_ros_tut/src/diffdrivebot
+# Build directory: /home/pi/Documents/Diff_driving_bot_tut_arduino/diff_driver_ros_tut/build/diffdrivebot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
