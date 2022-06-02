ca980de44afe0dfd47b2ffe0a068bf975cec7d19
zapster27 <tilzw27@gmail.com>
2022-06-02 04:06:23 -0700

    code for lidar

---
+++ diff_driver_ros_tut/build/diff_drive_bot_with_lidar/CMakeFiles/lidar_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/pi/Documents/Diff_driving_bot_tut_arduino/diff_driver_ros_tut/devel/lib/liblidar_plugin.pdb"
+  "/home/pi/Documents/Diff_driving_bot_tut_arduino/diff_driver_ros_tut/devel/lib/liblidar_plugin.so"
+  "CMakeFiles/lidar_plugin.dir/plugins/lidar_plugin.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lidar_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
